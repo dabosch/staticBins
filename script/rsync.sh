c5bf09795527d73c62d4d70a5dcc168b6ccc1715
Dario Bosch <dario.bosch@tuebingen.mpg.de>
2024-04-01 09:02:53 +0200

    fixed rsync: now compiled using musl

--- script/rsync.sh
+++ script/rsync.sh
@@ -5,8 +5,8 @@ URL="https://github.com/WayneD/rsync"
 git clone --depth 1 --branch "${VERSION}" "${URL}"
 
 cd rsync
-./configure --enable-static
-make LDFLAGS=-static
+CC=musl-gcc ./configure CFLAGS="-static" --disable-xxhash --disable-openssl --disable-zstd --disable-lz4
+make
 strip ./rsync
 upx ./rsync
 mv ./rsync ../bin/
